cb1f2e4c6a25ac4d53f9b232989440fc281718aa
Liam Baker <liam.baker2310@gmail.com>
2025-01-02 22:58:00 +0900

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { ClientLayout } from './ClientLayout'
+import Script from 'next/script'
+import { GoogleAnalytics } from './_components/analytics'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -19,6 +21,9 @@ export const metadata: Metadata = {
     'A modern platform providing predictive analytics for visa application processing timelines in Japan. Featuring interactive charts and user-friendly input fields for precise and personalized insights.',
 }
 
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID
+const isProduction = process.env.NODE_ENV === 'production'
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -26,9 +31,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {isProduction && (
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            strategy="afterInteractive"
+          />
+        )}
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GoogleAnalytics
+          measurementId={GA_TRACKING_ID}
+          isProduction={isProduction}
+        />
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
